921a66783a27836b927afbecafe6708eeea9486c
Alexander Pervushin <alex@pervush.in>
2022-07-20 00:39:34 +0300

    Fix. VotesMapper getVotes SQL. Missing task_uuid filter.

--- src/main/java/in/pervush/poker/repository/postgres/VotesMapper.java
+++ src/main/java/in/pervush/poker/repository/postgres/VotesMapper.java
@@ -14,9 +14,11 @@ import java.util.UUID;
 
 public interface VotesMapper {
 
-    @Insert("insert into votes(task_uuid, user_uuid, vote, create_dtm) " +
-            "values(#{taskUuid}, #{votingUserUuid}, cast(#{voteValue} as task_vote), #{createDtm})" +
-            "on conflict(task_uuid, user_uuid) do update set vote = excluded.vote, create_dtm = excluded.create_dtm")
+    @Insert("""
+            insert into votes(task_uuid, user_uuid, vote, create_dtm)
+            values(#{taskUuid}, #{votingUserUuid}, cast(#{voteValue} as task_vote), #{createDtm})
+            on conflict(task_uuid, user_uuid) do update set vote = excluded.vote, create_dtm = excluded.create_dtm
+            """)
     void createVote(@Param("taskUuid") UUID taskUuid,
                     @Param("votingUserUuid") UUID votingUserUuid,
                     @Param("voteValue") VoteValue vote,
@@ -26,9 +28,12 @@ public interface VotesMapper {
             @Arg(column = "user_name", javaType = String.class),
             @Arg(column = "vote", javaType = VoteValue.class)
     })
-    @Select("select u.name as user_name, v.vote " +
-            "from votes v " +
-            "inner join users u on u.user_uuid = v.user_uuid " +
-            "order by vote, user_name")
+    @Select("""
+            select u.name as user_name, v.vote
+            from votes v
+            inner join users u on u.user_uuid = v.user_uuid
+            where v.task_uuid = #{taskUuid}
+            order by vote, user_name
+            """)
     List<DBVote> getVotes(@Param("taskUuid") UUID taskUuid);
 }
